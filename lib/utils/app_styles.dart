215f07d94c9dee1fa996449afb6ee22d66bab0cb
samih1988 <140760407+samih1988@users.noreply.github.com>
2026-09-21 13:24:21 +0300

    Update app_styles.dart

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -4,9 +4,51 @@ import 'package:google_fonts/google_fonts.dart';
 import 'app_colors.dart';
 
 class AppStyles {
-  static TextStyle semi20black = GoogleFonts.poppins(
+  static TextStyle medium20black = GoogleFonts.inter(
     fontSize: 20,
-    fontWeight: FontWeight.w600,
+    fontWeight: FontWeight.w500,
     color: AppColors.black,
   );
+    static TextStyle medium20bwhite = GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.white,
+  );
+  static TextStyle medium14black = GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+  static TextStyle medium14bwhite = GoogleFonts.inter(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.white,
+  );
+  static TextStyle medium24white = GoogleFonts.inter(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: AppColors.white,
+  );
+  static TextStyle medium24black = GoogleFonts.inter(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+    static TextStyle medium12gray = GoogleFonts.inter(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: AppColors.gray,
+  );
+
+     static TextStyle bold16black = GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.black,
+  );
+     static TextStyle bold16white = GoogleFonts.inter(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: AppColors.white,
+  );
+  
 }
