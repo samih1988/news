76b8792bfdb8e7f96995d7a880d27635fb0e409e
samih1988 <140760407+samih1988@users.noreply.github.com>
2026-09-21 13:16:31 +0300

    Update app_colors.dart

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color white = Color(0xffFFFFFF);
-  static const Color black = Color(0xff1C1C1C);
-  static const Color grey = Color(0xff686868);
+  static const Color black = Color(0xff171717);
+  static const Color grey = Color(0xffA0A0A0);
   static const Color red = Color(0xffFF3232);
   static const Color transparentColor = Colors.transparent;
 }
